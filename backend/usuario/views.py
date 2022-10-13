a33f5b7336def4825827677fa6b2b3bac879416d
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-10-13 17:17:49 -0300

    Adicionando rota get de usuarios

--- backend/usuario/views.py
+++ backend/usuario/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST
+)
 
-# Create your views here.
+from usuario.models import Usuario
+from usuario.serializers import UsuarioSerializers
+
+class UsuarioApiView(APIView):
+    def get(self, request, format = None):
+        allUsuario = Usuario.objects.all()
+        serializers = UsuarioSerializers(allUsuario, many = True)
+        return Response(serializers.data, status = HTTP_200_OK)
